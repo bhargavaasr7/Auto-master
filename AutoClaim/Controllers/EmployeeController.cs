12287817a42e5802bee6280e716fcc012c3e03ce
bhargava sankarapu <bhargavaasr7@outlook.com>
2019-05-23 09:53:30 +0530

    Add project files.

---
+++ AutoClaim/Controllers/EmployeeController.cs
@@ -0,0 +1,53 @@
+﻿using AutoClaim.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoClaim.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private ApplicationDbContext dbContext = null;
+        public EmployeeController()
+        {
+            dbContext = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            dbContext.Dispose();
+        }
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(RegisteredUser registeredUser)
+        {
+            var user = dbContext.RegisteredUsers.SingleOrDefault(c => c.UserName == registeredUser.UserName && c.Password == registeredUser.Password);
+            if (user!=null)
+            {
+                if (user.UserName.StartsWith("AS"))
+                {
+                    return RedirectToAction("Index", "Assessor");
+                }
+               else if (user.UserName.StartsWith("CO"))
+                {
+                    return RedirectToAction("Index", "ClaimOfficer");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Employee");
+            }
+            return View();
+        }
+    }
+}
